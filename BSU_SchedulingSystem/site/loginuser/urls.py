23a4368bf8066b7ca4161c3e8704c9ffd88b5e6c
David Jude <judedavid0312@gmail.com>
2023-03-06 21:37:37 +0800

    some changes

--- BSU_SchedulingSystem/site/loginuser/urls.py
+++ BSU_SchedulingSystem/site/loginuser/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('change_password', views.change_password, name='change_password'),
+    path('add_instructor', views.add_instructor, name='add_instructor'),
+    path('add_student', views.add_student, name='add_student'),
     path('logout', views.logout, name='logout'),
 ]
